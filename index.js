4cde30c843ca25cd4194e4a69dbb61e7a3c9cc19
OsiaruRobert <osiarurbert@gmail.com>
2026-05-19 21:47:02 +0100

    LOOKs good on mobile & url/ => index.html

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import aiController from "./aiController.js";
 
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(express.json());
 
 app.use(express.static('public'));   // serve files from the "public" folder
 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.post("/api/chat", aiController);
-app.post("/api/test", (req,res)=>res.json({"reply":req.body.question}));
+app.post("/api/test", (req, res) => res.json({ "reply": req.body.question }));
 
 app.use((req, res) => res.status(404).json({ error: "Endpoint not found" }));
 
